f497c4a0c18b43a69a5de547fa879bba1fd5cb21
taksujin <161929037+taksujin@users.noreply.github.com>
2024-07-18 19:30:58 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,21 +1,21 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
     }
     if (receivedNumber == 2) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 20)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 10)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 25)
     }
     if (receivedNumber == 3) {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 20)
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 25)
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 10)
     }
     if (receivedNumber == 4) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 30)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CCW, 15)
     }
     if (receivedNumber == 5) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
     }
 })
-radio.setGroup(1)
+radio.setGroup(2)
 basic.showIcon(IconNames.Heart)
